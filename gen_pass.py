06a7104fdefd253ed7229bdf0a4670c118f33fe6
kra53n <59062496+Krai53n@users.noreply.github.com>
2020-06-24 11:09:28 +0300

    Update gen_pass.py

--- gen_pass.py
+++ gen_pass.py
@@ -1,20 +1,29 @@
 # program generate password
 # author: kra53n
-# date: 32.06.20
+# date: 23.06.20
 
 from random import randrange, choice
 
 def main():
+    '''
+    Function which run all program
+    '''
+    # lenght of password
     symbols = digit('Сколько симовлов ты хочешь иметь? ')
     symbols = int(symbols)
+    # first letter with upper or lower
     f_upper = ask_yes_no('Хочешь, чтобы первая буква была заглавной?')
+    # digits in password or not
     w_dig = ask_yes_no('Хочешь, чтобы у тебя были цифры?')
     password = ''
 
+    # generate password
     for i in range(symbols):
         if i == 0:
             if f_upper == 'y':
                 password += (gen_letter()).upper()
+            else:
+                password += (gen_letter()).lower()
         else:
             password += gen_letter(w_dig)
 
@@ -24,9 +33,10 @@ def main():
 def gen_letter(digit = 'n'):
     letters = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
     letters = letters.split(',')
-    letter = randrange(10) + 1
-    letter = str(letter)
-    letters.append(letter)
+    if digit == 'y':
+        letter = randrange(10) + 1
+        letter = str(letter)
+        letters.append(letter)
     letter = choice(letters)
     
     cap = randrange(2)
@@ -35,8 +45,7 @@ def gen_letter(digit = 'n'):
     return letter
 
 def ask_yes_no(question):
-    yes_no = None
-    while yes_no != 'y' or 'n':
+    while 1:
         yes_no = input('{} (y/n) '.format(question))
         yes_no = yes_no.lower()
         if yes_no == 'y' or 'n':
