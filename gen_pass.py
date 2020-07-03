ec38e0067efb0a6a07540b7151ea08dc3780f090
kra53n <59062496+Krai53n@users.noreply.github.com>
2020-07-03 10:21:40 +0300

    Update gen_pass.py

--- gen_pass.py
+++ gen_pass.py
@@ -1,40 +1,12 @@
-# program generate password
-# author: kra53n
-# date: 23.06.20
-
-
-from random import randrange, choice
+from random import choice, randrange
 from string import digits, ascii_lowercase
 
 
-def main():
-    '''
-    Function which run all program
-    '''
-    # lenght of password
-    symbols = digit('Сколько симовлов ты хочешь иметь? ')
-    symbols = int(symbols)
-    # first letter with upper or lower
-    f_upper = ask_yes_no('Хочешь, чтобы первая буква была заглавной?')
-    # digits in password or not
-    w_dig = ask_yes_no('Хочешь, чтобы у тебя были цифры?')
-    password = ''
-
-    # generate password
-    for i in range(symbols):
-        if i == 0:
-            if f_upper == 'y':
-                password += (gen_letter()).upper()
-            else:
-                password += (gen_letter()).lower()
-        else:
-            password += gen_letter(w_dig)
-
-    print('\nПароль уже сгенерирован, и он внизу:')
-    print(password)
-
-
 def gen_letter(digit = 'n'):
+    '''
+    Function generate random letter and give this letter to
+    password function
+    '''
     letters = ascii_lowercase
     if digit == 'y':
         letter = digits
@@ -48,21 +20,18 @@ def gen_letter(digit = 'n'):
     return letter
 
 
-def ask_yes_no(question):
-    while 1:
-        yes_no = input('{} (y/n) '.format(question))
-        yes_no = yes_no.lower()
-        if yes_no == 'y' or 'n':
-            break
-    return yes_no
+def password(symbols = 15, first_letter = 'y', digits = 'y'):
+    '''
+    Function generate password with your length of symbols,
+    capital letter in the start of password, digits if you whant
+    '''
+    pas = ''
+    for i in range(symbols):
+        if i == 0:
+            pas += gen_letter()
+            if first_letter == 'y':
+                pas = pas.upper()
+            continue
+        pas += gen_letter(digits)
 
-
-def digit(question):
-    num = '1'
-    while num != int(num):
-        num = input(question)
-        num = int(num)
-    return num
-
-
-main()
+    return pas
